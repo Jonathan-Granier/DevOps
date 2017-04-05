85a1325215b7beb61f8b04c43a7d25684cb98cbd
Gabriel <gabriel.bouvier-denoix@etu.univ-grenoble-alpes.fr>
2017-04-05 10:37:52 +0200

    TestreqH plus de orj.junit

--- src/test/java/main/Test_req_handler.java
+++ src/test/java/main/Test_req_handler.java
@@ -1,42 +1,75 @@
 package test.java.main;
 
-import static org.junit.Assert.*;
 
-import org.junit.Before;
-import org.junit.Test;
+import java.util.Random;
 
-import exception.WrongDataTypeException;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
-public class Test_req_handler {
+import main.java.base_de_donnees.BaseDeDonnees;
+import main.java.exception.WrongDataTypeException;
+import main.java.stockage_cle_valeur.RequestHandler;
+
+public class Test_req_handler extends TestCase {
+	RequestHandler req_H;
+	private static int nb_iter=15;
 	
-	public class Test_add {
-		//Main m;
-		@org.junit.Test
-		public void test() {
+	public Test_req_handler( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return (Test) new TestSuite( AddTest.class );
+    }
+
+		
+		
+	//@Before
+	public void setUp()
+	{
+		//m = new Main();
+		req_H = new RequestHandler(new BaseDeDonnees());
+	}
+	
+	
+	
+	//@Test//(expected=WrongDataTypeException.class)
+	public void test_atomic_function_wrong_data_type(){
+		//Add une data d'un type
+		//tente une fonction d'un type non correspondant
+		assertTrue(true);
+	}
+	
+	//@Test
+	public void test_add_get_key(){
+		Random r = new Random();
+		int value,key,iter=0;
+		boolean test=true;
+		
+		while(iter < nb_iter && test){
+			value = r.nextInt();
+			key = r.nextInt();
+			req_H.add(key, value);
+			test = (value == (int)req_H.get(key));
+			iter ++;
 		}
-		
-		
-		@Before
-		public void setUp()
-		{
-			//m = new Main();
-		}
-		
-		
-		@Test
-		public void test_Add() {
-			//assertTrue( m.add(2,2)==4);
-		}
-		
-		@Test//(expected=WrongDataTypeException.class)
-		public void test_atomic_function_wrong_data_type(){
-			//Add une data d'un type
-			//tente une fonction d'un type non correspondant
-			assertTrue(true);
-		}
-		
-		//public void 
+		assertTrue(test);
 		
 	}
-
+	
+    /**
+     * Rigourous Test :-)
+     */
+    public void test_Add()
+    {
+        assertTrue( true );
+    }
 }
+
+
