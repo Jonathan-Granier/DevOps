43097f90c1dfee518904051fb80d9f68657cc76f
Astor-Bizard <astor.bizard@etu.univ-grenoble-alpes.fr>
2017-04-05 09:27:37 +0200

    ajout de stockage_cle_valeur

---
+++ src/stockage_cle_valeur/RequestHandler.java
@@ -0,0 +1,21 @@
+package stockage_cle_valeur;
+
+import java.util.Hashtable;
+
+public class RequestHandler {
+
+	Hashtable<Integer,Object> H;
+	
+	public RequestHandler(){
+		H = new Hashtable<Integer,Object>();
+	}
+	
+	public void add(Integer cle, Object valeur){
+		System.out.println("Ajout de " + valeur.toString() + " avec la cle " + cle);
+	}
+	
+	public Object get(Integer cle){
+		System.out.println("Acces a " + cle);
+		return null;
+	}
+}
