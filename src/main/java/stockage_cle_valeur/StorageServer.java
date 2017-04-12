1e6dd945daca840219ac992dbc65291b3f00eebb
Astor-Bizard <astor.bizard@etu.univ-grenoble-alpes.fr>
2017-04-12 09:09:57 +0200

    Aye y'a le StorageServer avec evinceLRU

--- src/main/java/stockage_cle_valeur/StorageServer.java
+++ src/main/java/stockage_cle_valeur/StorageServer.java
@@ -1,6 +1,7 @@
 package main.java.stockage_cle_valeur;
 
 import java.util.Hashtable;
+import java.util.LinkedList;
 import java.util.Random;
 
 import main.java.exception.NonExistingKeyException;
@@ -19,6 +20,8 @@ public class StorageServer implements StorageServerInterface{
 	
 	private final int ID;
 	private Hashtable<Object,Object> H;
+	private LinkedList<Object> keys_usage_order;
+	private LinkedList<Object> values_usage_order;
 	private final int capacity;
 	
 	/**
@@ -28,8 +31,18 @@ public class StorageServer implements StorageServerInterface{
 		ID = nb_servers;
 		nb_servers++;
 		H = new Hashtable<Object,Object>();
+		keys_usage_order = new LinkedList<Object>();
+		values_usage_order = new LinkedList<Object>();
 		Random r = new Random();
 		capacity = ((r.nextInt()%(2*MARGE))-MARGE) + AVG_CAPACITY;
+	}
+	
+	/**
+	 * Renvoie l'ID du serveur (unique pour tout StorageServer)
+	 * @return l'ID du serveur
+	 */
+	public int getID(){
+		return ID;
 	}
 
 	/**
@@ -52,28 +65,57 @@ public class StorageServer implements StorageServerInterface{
 	 * Supprime l'element le moins recemment utilise
 	 * @return false ssi le serveur etait vide
 	 */
-	public boolean evinceLRU() {
-		// TODO Auto-generated method stub
-		return false;
+	public void evinceLRU() {
+		values_usage_order.pop();
+		H.remove(keys_usage_order.pop());
 	}
 
 	public boolean contains(Object elem) {
-		return H.contains(elem);
+		if(H.contains(elem)){
+			int index_of_elem = values_usage_order.indexOf(elem);
+			refreshAtIndex(index_of_elem);
+			return true;
+		}
+		else
+			return false;
 	}
 
 	public boolean containsKey(Object key) {
-		return H.containsKey(key);
+		if(H.containsKey(key)){
+			int index_of_key = keys_usage_order.indexOf(key);
+			refreshAtIndex(index_of_key);
+			return true;
+		}
+		else
+			return false;
 	}
 
 	public Object get(Object key) throws NonExistingKeyException{
 		Object res = H.get(key);
 		if(res == null)
 			throw new NonExistingKeyException();
-		else
+		else{
+			int index_of_elem = values_usage_order.indexOf(res);
+			refreshAtIndex(index_of_elem);
 			return res;
+		}
 	}
 
 	public void put(Object key, Object elem) {
 		H.put(key, elem);
+		keys_usage_order.add(key);
+		values_usage_order.add(elem);
+	}
+
+	public void remove(Object key) {
+		H.remove(key);
+		int index_of_key = keys_usage_order.indexOf(key);
+		keys_usage_order.remove(index_of_key);
+		values_usage_order.remove(index_of_key);
+	}
+	
+	private void refreshAtIndex(int index){
+		keys_usage_order.add(keys_usage_order.remove(index));
+		values_usage_order.add(values_usage_order.remove(index));
 	}
 }
--- src/main/java/stockage_cle_valeur/StorageServerInterface.java
+++ src/main/java/stockage_cle_valeur/StorageServerInterface.java
@@ -9,12 +9,15 @@ import main.java.exception.NonExistingKeyException;
  */
 public interface StorageServerInterface {
 	
+	public int getID();
+	
 	public int getCapacity();
 	public boolean isFull();
-	public boolean evinceLRU();
+	public void evinceLRU();
 	
 	public boolean contains(Object elem);
 	public boolean containsKey(Object key);
 	public Object get(Object key) throws NonExistingKeyException;
 	public void put(Object key, Object elem);
+	public void remove(Object key);
 }
