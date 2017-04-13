807045db19aed5b60a6c9bd7019154a41fddcd36
Astor-Bizard <astor.bizard@etu.univ-grenoble-alpes.fr>
2017-04-13 15:23:46 +0200

    baaaaaaaaaaaaa des tests.

--- src/test/java/main/RequestHandlerTest.java
+++ src/test/java/main/RequestHandlerTest.java
@@ -3,16 +3,15 @@ package test.java.main;
 import org.junit.*;
 
 import main.java.base_de_donnees.BaseDeDonnees;
+import main.java.commande_structure.Answer;
+import main.java.commande_structure.Answer.returnCode;
 import main.java.commande_structure.Request;
 import main.java.commande_structure.Request.opCode;
-import main.java.exception.NonExistingKeyException;
 import main.java.stockage_cle_valeur.RequestHandler;
 import main.java.stockage_cle_valeur.ServerManager;
 import main.java.stockage_cle_valeur.StorageServer;
 
 import static org.junit.Assert.*;
-
-import java.util.ArrayList;
 
 /**
  * Test fonctionnel : verifie que les donnees sont coherentes pour le client
@@ -40,11 +39,87 @@ public class RequestHandlerTest{
     }
 
 	@Test
+	public void test_setInt(){
+		helper_set(opCode.setInt,"Reponse a la vie, l'univers et le reste",42);
+	}
+
+	@Test
 	public void test_setString(){
+		helper_set(opCode.setString,"Bonjour le monde","Hello world !");
+	}
+	
+	@Test
+	public void test_setObject(){
+		helper_set(opCode.setObject,"Un objet",this);
+	}
+	
+	private void helper_set(opCode code, String key, Object data){
 		req.reqNumber = reqNum;
-		req.op_code = opCode.setString;
-		req.key = "Bonjour le monde";
-		req.data = "Hello world !";
+		req.op_code = code;
+		req.key = key;
+		req.data = data;
+		Answer ans = rqHdl.handleRequest(req);
+		assertEquals(returnCode.OK,ans.return_code);
+		assertEquals(reqNum,ans.reqNumber);
+	}
+	
+	@Test
+	public void test_setInt_wrongType(){
+		helper_set_wrongType(opCode.setInt,"Est-ce un entier ?","Ce n'est pas un entier");
+	}
+	
+	@Test
+	public void test_setString_wrongType(){
+		helper_set_wrongType(opCode.setString,"Est-ce une chaine de caracteres ?",false);
+	}
+	
+	@Test
+	public void test_setObject_wrongType(){
+		helper_set_wrongType(opCode.setObject,"Let's fool rqHdl",666);
+	}
+	
+	private void helper_set_wrongType(opCode code, String key, Object data){
+		req.reqNumber = reqNum;
+		req.op_code = code;
+		req.key = key;
+		req.data = data;
+		Answer ans = rqHdl.handleRequest(req);
+		assertEquals(returnCode.WrongDataType,ans.return_code);
+		assertEquals(reqNum,ans.reqNumber);
+	}
+
+	@Test
+	public void test_setGetInt(){
+		helper_setGet(opCode.setInt,"1",42);
+	}
+	
+	@Test
+	public void test_setGetSrting(){
+		helper_setGet(opCode.setString,"2","oui");
+	}
+	
+	@Test
+	public void test_setGetObject(){
+		helper_setGet(opCode.setObject,"3",this);
+	}
+	
+	private void helper_setGet(opCode code, String key, Object data){
+		req.reqNumber = reqNum;
+		req.op_code = code;
+		req.key = key;
+		req.data = data;
+		rqHdl.handleRequest(req);
+
+		reqNum++;
+		
+		req.reqNumber = reqNum;
+		req.op_code = opCode.get;
+		req.key = key;
+		req.data = null;
+		Answer ans = rqHdl.handleRequest(req);
+		assertEquals(returnCode.OK,ans.return_code);
+		assertEquals(reqNum,ans.reqNumber);
+		assertEquals(ans.data,data);
 	}
 	
 	@After
