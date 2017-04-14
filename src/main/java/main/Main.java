7cf13c0aa8f278f38fdc240e84c7769578e92149
Astor <Astor@192.168.1.19>
2017-04-14 15:10:53 +0200

    Fix du main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,15 +3,17 @@ package main.java.main;
 import main.java.base_de_donnees.BaseDeDonnees;
 import main.java.exception.NonExistingKeyException;
 import main.java.stockage_cle_valeur.ServerManager;
+import main.java.stockage_cle_valeur.StorageServer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		ServerManager svMgr = new ServerManager();
 		svMgr.changeBDD(new BaseDeDonnees());
+		svMgr.addServer(new StorageServer());
 		svMgr.add(42, 23);
 		try {
-			System.out.println("Bonjour, ça doit faire 23 : " + svMgr.get(42));
+			System.out.println("Bonjour, ca doit faire 23 : " + svMgr.get(42));
 		} catch (NonExistingKeyException e) {
 			e.printStackTrace();
 		}
