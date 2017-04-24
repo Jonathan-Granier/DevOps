6a849dbd6f61387f7d4a5d1cb48a3a84aeee75dd
Jonathan-Granier <jonathan.granier@wanadoo.fr>
2017-04-24 09:14:31 +0200

    Squelette de test pour les echange client serveur avec le traducteur

---
+++ src/test/java/main/EchangeClientServeurTest.java
@@ -0,0 +1,108 @@
+package test.java.main;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+/**
+ * Test fonctionnelle : Test si les données avant et après un echange client serveur sont equivalente.
+ * @author granijon
+ *
+ */
+
+public class EchangeClientServeurTest {
+	@Before
+	public void init(){
+    
+    }
+	
+	
+	
+	
+	/*-------------------------------Traduction Seulement -------------------------------*/
+	@Test
+	public void test_ClientToServer_Traduction_1()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ClientToServer_Traduction_2()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ClientToServer_Traduction_3()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ServerToClient_Traduction_1()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ServerToClient_Traduction_2()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ServerToClient_Traduction_3()
+	{
+		assertTrue(true);
+	}
+
+	
+	/*-------------------------------Echange l'un vers l'autre -------------------------------*/
+	
+	
+	@Test
+	public void test_ClientToServer_Echange_1()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ClientToServer_Echange_2()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ClientToServer_Echange_3()
+	{
+		assertTrue(true);
+	}
+	
+	
+	
+	@Test
+	public void test_ServerToClient_Echange_1()
+	{
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test_ServerToClient_Echange_2()
+	{
+		assertTrue(true);
+	}
+
+	
+	@Test
+	public void test_ServerToClient_Echange_3()
+	{
+		assertTrue(true);
+	}
+
+
+
+
+	
+	
+}
